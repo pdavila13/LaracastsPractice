753444c769743ebbd76c88487625dfadafc5e931
Paolo Davila <paolodavila@iesebre.com>
2015-10-18 23:29:53 +0200

    Passing data to views. Version 0.1

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,7 +10,9 @@ class PagesController extends Controller
 {
     public function about()
     {
-        return view('pages.about');
+        $name = 'Paolo Dávila';
+
+        return view('pages.about')->with('name', $name);
         //return 'About Page';
     }
 }
